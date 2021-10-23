62b63c671417070c2d04d9a12f4a7af7d8b84ebe
Yishai Zehavi <zehaviyishai@gmail.com>
2021-10-23 22:23:40 +0300

    fix indentation

--- day-1/src/App.svelte
+++ day-1/src/App.svelte
@@ -1,17 +1,15 @@
 <script>
-	import 'virtual:windi.css';
-	import ListItem from './ListItem.svelte';
+  import 'virtual:windi.css';
+  import ListItem from './ListItem.svelte';
 </script>
 
-<main class="h-screen w-screen grid place-content-center">
-	<div class="text-sm font-medium mb-3 underline">Animals</div>
-	<ul class="w-72 grid gap-y-3">
-		{#each ['Zebra', 'Lion', 'Dog'] as animal}
-			<ListItem
-				class="w-full"
-				value={animal} />
-		{/each}
-	</ul>
+<main class="windi-o7kgup">
+  <div class="windi-1bxy4xb">Animals</div>
+  <ul class="windi-d0lu4u">
+    {#each ['Zebra', 'Lion', 'Dog'] as animal}
+    <ListItem class="windi-10cf8i0" value={animal} />
+    {/each}
+  </ul>
 </main>
 
 <style windi:global windi:preflights:global windi:safelist:global>
