7f4e8dac26353dd6896664f71c05ae62c707dc0a
Yishai Zehavi <zehaviyishai@gmail.com>
2021-10-23 22:23:59 +0300

    implement view mode and edit mode

--- day-1/src/ListItem.svelte
+++ day-1/src/ListItem.svelte
@@ -5,16 +5,37 @@
 
   // mode toggle
   export let editMode = false;
-  const moveToEditMode = () => editMode = true;
-  const moveToDisplayMode = () => editMode = false;
+  const moveToEditMode = () => (editMode = true);
+  const moveToDisplayMode = () => (editMode = false);
 </script>
 
-<input
-  type="text"
-  class="{$$props.class} appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none cursor-default focus:cursor-text"
+<div
+  class="flex items-center border-2 rounded pointer-events-auto group"
   class:border-blue-400={editMode}
   class:border-transparent={!editMode}
-  bind:value={tempValue}
   on:click={moveToEditMode}
-  on:blur={moveToDisplayMode}
-  readonly={!editMode || null}>
+>
+  {#if $$slots.start}
+    <div class="pl-3">
+      <slot name="start" />
+    </div>
+  {/if}
+  <input
+    type="text"
+    class="{$$props.class} default-styles group-focus:cursor-text"
+    bind:value={tempValue}
+    on:blur={moveToDisplayMode}
+    readonly={!editMode || null}
+  />
+  {#if $$slots.end}
+    <div class="pr-3">
+      <slot name="end" />
+    </div>
+  {/if}
+</div>
+
+<style scoped>
+  .default-styles {
+    @apply appearance-none w-full py-2 px-3 text-gray-700 leading-tight cursor-default outline-none;
+  }
+</style>
